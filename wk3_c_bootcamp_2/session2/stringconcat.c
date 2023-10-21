f2c20dba84652d2c6f02b39b47e4c659446d5ed3
prlarkin04 <146266299+prlarkin04@users.noreply.github.com>
2023-10-21 16:09:27 +0000

    Stringconcat started

--- wk3_c_bootcamp_2/session2/stringconcat.c
+++ wk3_c_bootcamp_2/session2/stringconcat.c
@@ -4,3 +4,17 @@ void stringconcat(char string1, char string2)
 {
     
 }
+
+int main()
+{
+    char string1[50];
+    char string2[50];
+
+    printf("Enter first string:");
+    scanf("%s", string1);
+    printf("Enter second string:");
+    scanf("%s", string2);
+
+    printf("The concatenated string is %s\n", stringconcat(string1, string2));
+    
+}
