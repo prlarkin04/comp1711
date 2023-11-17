e8c019bee2a30d8ed7bbf97d04c91b515d33d408
prlarkin04 <146266299+prlarkin04@users.noreply.github.com>
2023-11-17 14:39:49 +0000

    Task 2 work

--- CW2/StepCounter_Final.c
+++ CW2/StepCounter_Final.c
@@ -47,44 +47,73 @@ int main()
 {
 
     char option;
-    bool optionvalid = false;
+    int optionvalid = 0;
+    char filename[500];
 
-    printf ("\n");
-    printf ("Menu Options:\n");
-    printf ("A: Specify the filename to be imported\n");
-    printf ("B: Display the total number of records in the file\n");
-    printf ("C: Find the date and time of the timeslot with the fewest steps\n");
-    printf ("D: Find the date and time of the timeslot with the largest number of steps\n");
-    printf ("E: Find the mean step count of all the records in the file\n");
-    printf ("F: Find the longest continuous period where the step count is above 500 steps\n");
-    printf ("Q: Quit\n");
-
-    scanf(" %c", &option);
-
-    switch (option)
+    while (optionvalid == 0)
     {
-        case 'A':
-        case 'a':
-            printf ("Converted to degrees Celsius: \n");
+        printf ("\n");
+        printf ("Menu Options:\n");
+        printf ("A: Specify the filename to be imported\n");
+        printf ("B: Display the total number of records in the file\n");
+        printf ("C: Find the date and time of the timeslot with the fewest steps\n");
+        printf ("D: Find the date and time of the timeslot with the largest number of steps\n");
+        printf ("E: Find the mean step count of all the records in the file\n");
+        printf ("F: Find the longest continuous period where the step count is above 500 steps\n");
+        printf ("Q: Quit\n");
+        printf ("Enter Choice: ");
+
+        scanf(" %c", &option);
+
+        switch (option)
+        {
+            case 'A':
+            case 'a':
+                printf ("Input Filename: ");
+                scanf("%s", filename);
+                FILE *input = fopen(filename, "r");
+                if (input == NULL) 
+                {
+                    printf("Error: Could not find or open the file.\n");
+                } 
+                break;
+
+            case 'B':
+            case 'b':
+                printf ("Total Records: ");
+                break;
+
+            case 'C':
+            case 'c':
+                printf("Fewest Steps: ");
+                break;
+            
+            case 'D':
+            case 'd':
+                printf("Largest Steps: ");
+                break;
+
+            case 'E':
+            case 'e':
+                printf("Mean Steps:");
+                break;
+
+            case 'F':
+            case 'f':
+                printf("Longest Period Start: \n");
+                printf("Longest Period End: ");
+                break;
+
+
+            case 'Q':
+            case 'q':
+                optionvalid = 1;
                     break;
-
-        case 'B':
-        case 'b':
-            printf ("Converted to Kelvin: \n");
-                  break;
-
-        case 'C':
-        case 'c':
-            printf("Converted to Newton: \n");
-                break;
-
-        case 'Q':
-        case 'q':
-            break;
-        
-        default:
-            printf("Invalid option. Please enter a valid option.\n");
-                break;
+            
+            default:
+                printf("Invalid option. Please enter a valid option.\n");
+                    break;
+        }
     }  
 }
 
