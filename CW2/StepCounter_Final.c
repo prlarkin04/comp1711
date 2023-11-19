2e8f9c4ec75642301888de36228e15df0b590ee3
prlarkin04 <146266299+prlarkin04@users.noreply.github.com>
2023-11-19 03:06:41 +0000

    Finished task 2

--- CW2/StepCounter_Final.c
+++ CW2/StepCounter_Final.c
@@ -6,9 +6,13 @@
 // Struct moved to header file
 
 // Define any additional variables here
+int counter = 0;
 
 
 // Global variables for filename and FITNESS_DATA array
+FITNESS_DATA data[100];
+char filename[500];
+
 
 
 // This is your helper function. Do not change it in any way.
@@ -48,7 +52,24 @@ int main()
 
     char option;
     int optionvalid = 0;
-    char filename[500];
+    int buffer_size = 250;
+    char line [buffer_size];
+
+    char date[11];
+    char time[6];
+    char step[100];
+    int feweststeps;
+    int indexfewest;
+    int largeststeps;
+    int indexlargest;
+    int steptotal;
+    int stepmean;
+    int longeststartindex = -1;
+    int longestendindex = -1;
+    int currentstartIndex = -1;
+    int currentendIndex = -1;
+
+    
 
     while (optionvalid == 0)
     {
@@ -69,6 +90,7 @@ int main()
         {
             case 'A':
             case 'a':
+                counter = 0;
                 printf ("Input Filename: ");
                 scanf("%s", filename);
                 FILE *input = fopen(filename, "r");
@@ -76,32 +98,106 @@ int main()
                 {
                     printf("Error: Could not find or open the file.\n");
                 } 
+                else
+                {
+                printf("File successfully loaded");
+                while(fgets(line, buffer_size, input))
+                {
+                    tokeniseRecord(line, ",",date,time,step);
+                    strcpy(data[counter].date,date);
+                    strcpy(data[counter].time,time);
+                    data[counter].steps = atoi(step);
+                    counter++;
+
+                }
+                }
                 break;
 
             case 'B':
             case 'b':
-                printf ("Total Records: ");
+                printf ("Total Records: %d\n", counter);
                 break;
 
             case 'C':
             case 'c':
-                printf("Fewest Steps: ");
+                feweststeps= data[0].steps; // Initialize with the first record's steps
+                indexfewest = 0; // Initialize index for the record with the fewest steps
+
+                for (int i = 1; i < counter; i++)
+                {
+                    if (data[i].steps < feweststeps)
+                    {
+                        feweststeps = data[i].steps;
+                        indexfewest = i;
+                    }
+                }
+                printf("Fewest Steps:  %s %s", data[indexfewest].date, data[indexfewest].time);
                 break;
             
             case 'D':
             case 'd':
-                printf("Largest Steps: ");
+                largeststeps= data[0].steps; // Initialize with the first record's steps
+                indexlargest = 0; // Initialize index for the record with the largest steps
+
+                for (int i = 1; i < counter; i++)
+                {
+                    if (data[i].steps > largeststeps)
+                    {
+                        largeststeps = data[i].steps;
+                        indexlargest = i;
+                    }
+                }
+                printf("Largest Steps:  %s %s", data[indexlargest].date, data[indexlargest].time);
                 break;
 
             case 'E':
             case 'e':
-                printf("Mean Steps:");
+                steptotal = 0;
+                for (int i = 0; i < counter; i++)
+                {
+                    steptotal=steptotal+data[i].steps;
+                }
+                stepmean = steptotal/counter;
+                printf("Mean step count: %d", stepmean);
                 break;
 
             case 'F':
             case 'f':
-                printf("Longest Period Start: \n");
-                printf("Longest Period End: ");
+
+                for (int i = 0; i < counter; i++) {
+                    if (data[i].steps > 500) {
+                        if (currentstartIndex == -1) {
+                            // Found the start of a new period
+                            currentstartIndex = i;
+                        }
+                        // Update the end index of the current period
+                        currentendIndex = i;
+                    } else {
+                        // Check if the current period is longer than the longest found so far
+                        if (currentendIndex - currentstartIndex > longestendindex - longeststartindex) {
+                            // Update the longest period indices
+                            longeststartindex = currentstartIndex;
+                            longestendindex = currentendIndex;
+                        }
+                        // Reset the current period indices to search for the next period
+                        currentstartIndex = -1;
+                        currentendIndex = -1;
+                    }
+                }
+
+                // Check if the last period is longer than the longest found so far
+                if (currentendIndex - currentstartIndex > longestendindex - longeststartindex) {
+                    longeststartindex = currentstartIndex;
+                    longestendindex = currentendIndex;
+                }
+
+                // Print the start and end dates and times of the longest continuous period
+                if (longeststartindex != -1 && longestendindex != -1) {
+                    printf("Longest Period Start: %s %s\n", data[longeststartindex].date, data[longeststartindex].time);
+                    printf("Longest Period End: %s %s", data[longestendindex].date, data[longestendindex].time);
+                } else {
+                    printf("No period with steps above 500 found.\n");
+                }
                 break;
 
 
@@ -111,7 +207,7 @@ int main()
                     break;
             
             default:
-                printf("Invalid option. Please enter a valid option.\n");
+                printf("Invalid choice. Try again.");
                     break;
         }
     }  
